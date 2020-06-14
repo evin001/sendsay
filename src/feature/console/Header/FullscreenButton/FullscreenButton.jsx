65b1cc55b5669fbb373c04fcd6692b8ed49fbc2e
Evgeniy <blednovea@smartguide.agency>
2020-06-14 15:47:45 +0300

    fullscreen styles and focus

--- src/feature/console/Header/FullscreenButton/FullscreenButton.jsx
+++ src/feature/console/Header/FullscreenButton/FullscreenButton.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react'
 import screenfull from 'screenfull'
+import { cn } from '@bem-react/classname'
 import FullscreenIcon from '../icons/FullscreenIcon'
 import FullscreenOutputIcon from '../icons/FullscreenOutputIcon'
+import './FullscreenButton.css'
+
+const classes = cn('FullscreenButton')
 
 const FullscreenButton = () => {
   const [open, setOpen] = useState(false)
+
   const handleToggle = () => {
     if (screenfull.isEnabled) {
       screenfull.toggle()
       setOpen(!open)
     }
   }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleToggle()
+    }
+  }
+
   return (
-    <div onClick={handleToggle}>
+    <div
+      tabIndex={0}
+      onClick={handleToggle}
+      onKeyPress={handleKeyPress}
+      className={classes('root')}
+    >
       {open ? <FullscreenOutputIcon /> : <FullscreenIcon />}
     </div>
   )
